bacca4c01d080f5a7d8e561cf4e486c12dac0e8a
ThomasPDM <thomas.pelissie@laposte.net>
2021-04-21 10:08:20 +0200

    runnable medmask detector program with spatial location

--- 3-use_custom_model/examples/coronamask/README.md
+++ 3-use_custom_model/examples/coronamask/README.md
@@ -24,13 +24,19 @@ Au niveau de la structure de la sortie du modèle, il s'agit donc de l'attribut 
 
 
 ## run_coronamask_depth : 
-Programme similaire mais avec estimation de la position dans l'espace à l'aide de l'objet générique [NeuralNetwork](https://docs.luxonis.com/projects/api/en/latest/references/python/#depthai.NeuralNetwork).
+Programme avec notion de profondeur, détecte les visages, indique si un masque est porté et donne la position dans l'espace. La position spatiale est obtenue grâce à une instance de l'objet [SpatialLocationCalculator](https://docs.luxonis.com/projects/api/en/latest/references/python/#depthai.SpatialLocationCalculator) et la détection se fait via l'objet générique [NeuralNetwork](https://docs.luxonis.com/projects/api/en/latest/references/python/#depthai.NeuralNetwork).
 
-(en cours d'écriture)
+Structure de la sortie du modèle :
+```json
+Liste de 100 zones d'intérêts potentielles, suivant cette structure :
+["id", "label", "confidence", "left", "top", "right", "bottom"]
+```
+
+Pour utiliser la profondeur, il suffit de passer les zones d'intérêts (ROI) dans la configuration de l'instance de SpatialLocationCalculator, ce qui renvoie une instance d'objet [SpatialLocationCalculatorData](https://docs.luxonis.com/projects/api/en/latest/references/python/#depthai.SpatialLocationCalculatorData) qui permet de récupérer les positions dans l'espace.
 <br><br>
 
 
 ## run_coronamask_mobilenet_depth : 
-Programme similaire mais avec estimation de la position dans l'espace à l'aide de l'objet [MobileNetSpatialDetectionNetwork](https://docs.luxonis.com/projects/api/en/latest/references/python/#depthai.MobileNetSpatialDetectionNetwork).
+Programme similaire mais avec estimation de la position dans l'espace à l'aide de l'objet [MobileNetSpatialDetectionNetwork](https://docs.luxonis.com/projects/api/en/latest/references/python/#depthai.MobileNetSpatialDetectionNetwork), globalement, l'objet est le même que MobileNetDetectionNetwork, sauf qu'ici, en plus du flux vidéo de la caméra rgb, il faut lier la capture de profondeur à l'entrée.
 
-(en cours d'écriture)
+Au niveau de la structure de la sortie du modèle, il s'agit donc de l'attribut [out](https://docs.luxonis.com/projects/api/en/latest/references/python/#depthai.SpatialDetectionNetwork.out) qui est un objet [SpatialImgDetections](https://docs.luxonis.com/projects/api/en/latest/references/python/#depthai.SpatialImgDetections)
---
+++ 3-use_custom_model/examples/coronamask/run_coronamask_mobilenet_depth.py
@@ -0,0 +1,102 @@
+import depthai as dai
+import time
+import cv2
+from pathlib import Path
+
+
+
+# Draw ROI and class label of each detected thing
+def frame_process(frame, detection):
+    topleft = (int(detection.xmin*frame_width), int(detection.ymin*frame_height))
+    bottomright = (int(detection.xmax*frame_width), int(detection.ymax*frame_height))
+    bottomleft = (int(detection.xmin*frame_width), int(detection.ymax*frame_height))
+    color = (255,0,0)
+    cv2.rectangle(frame, topleft, bottomright, color, 2) # ROI
+    cv2.putText(frame, labels[detection.label] + f" {int(detection.confidence * 100)}%", (topleft[0] + 10, topleft[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color) # Label and confidence
+    cv2.putText(frame, f"X: {int(detection.spatialCoordinates.x)} mm", (topleft[0] + 10, topleft[1] + 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+    cv2.putText(frame, f"Y: {int(detection.spatialCoordinates.y)} mm", (topleft[0] + 10, topleft[1] + 65), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+    cv2.putText(frame, f"Z: {int(detection.spatialCoordinates.z)} mm", (topleft[0] + 10, topleft[1] + 80), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+    return frame
+
+
+
+# Define program parameters
+nn_path = str(Path(__file__).parent) + "/model.blob" # path to the neural network compiled model (.blob)
+labels = ["background", "no mask", "mask", "no mask"]
+pipeline = dai.Pipeline()
+frame_width = 300
+frame_height = 300
+fps_limit = 20
+
+# Prepare depth handling
+depth = pipeline.createStereoDepth()
+
+# Set depth source
+left = pipeline.createMonoCamera()
+left.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+left.setBoardSocket(dai.CameraBoardSocket.LEFT)
+right = pipeline.createMonoCamera()
+right.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+right.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+
+
+# Set rgb camera source
+cam_rgb = pipeline.createColorCamera()
+cam_rgb.setPreviewSize(frame_width, frame_height)
+cam_rgb.setInterleaved(False)
+cam_rgb.setFps(fps_limit)
+
+
+# Configure neural network settings
+nn = pipeline.createMobileNetSpatialDetectionNetwork()
+nn.setConfidenceThreshold(0.5) # keep detections if confidence>50%
+nn.setBlobPath(nn_path)
+nn.setNumInferenceThreads(2)
+cam_rgb.preview.link(nn.input) # link cam_rgb to nn input layer
+
+
+# Set depth output stream
+left.out.link(depth.left)
+right.out.link(depth.right)
+depth.depth.link(nn.inputDepth)
+
+# Set rgb output stream
+rgb_output_stream = pipeline.createXLinkOut()
+rgb_output_stream.setStreamName("rgb")
+nn.passthrough.link(rgb_output_stream.input)
+
+# Set neural network output stream
+nn_output_stream = pipeline.createXLinkOut()
+nn_output_stream.setStreamName("nn")
+nn.out.link(nn_output_stream.input)
+
+
+
+with dai.Device(pipeline) as device:
+    device.startPipeline()
+    rgb_queue = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+    detection_queue = device.getOutputQueue(name="nn", maxSize=4, blocking=False)
+
+    frame = None
+    detections = []
+    startTime = time.monotonic() # To determined FPS
+    counter = 0
+
+
+    while True:
+        rgb_current_output = rgb_queue.get()
+
+        if rgb_current_output is not None:
+            frame = rgb_current_output.getCvFrame()
+            cv2.putText(frame, "NN fps: {:.2f}".format(counter / (time.monotonic() - startTime)), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color=(255, 255, 255))
+
+            detections = detection_queue.get().detections
+            for detection in detections:
+                frame = frame_process(frame, detection) # process the frame
+            counter += 1
+
+        if frame is not None:
+            cv2.imshow("output", frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
