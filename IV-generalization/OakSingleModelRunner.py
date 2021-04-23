0c0cd7f7696d71f7d2e8b423fe0b5c462b1e954c
ThomasPDM <thomas.pelissie@laposte.net>
2021-04-23 09:50:44 +0200

    handle depth with single model runner

--- IV-generalization/OakSingleModelRunner.py
+++ IV-generalization/OakSingleModelRunner.py
@@ -1,76 +1,106 @@
 import depthai as dai
-import time
 import cv2
 
 
 middle_cam_stream_name = "middle_cam"
 nn_stream_name = "nn"
+spatial_calculator_input_stream_name = "spatial_config"
+spatial_calculator_output_stream_name = "spatial_data"
 
 
 class OakSingleModelRunner:
     def __init__(self):
-        self.frame_width = None
-        self.frame_height = None
-        self.labels = None
-
+        self.middle_cam = None
+        self.left_cam = None
+        self.right_cam = None
+        self.stereo = None
+        self.nn = None
         self.middle_cam_output_queue = None
         self.nn_output_queue = None
 
+        self.labels = None
         self.__pipeline = dai.Pipeline()
-        self.__middle_cam = None
-        self.__nn = None
         
-
-    def setMiddleCamera(self, frame_width, frame_height, fps_limit=None):
+        
+    def setMiddleCamera(self, frame_width, frame_height):
         # Needed configuration
-        self.__middle_cam = self.__pipeline.createColorCamera()
-        self.__middle_cam.setPreviewSize(frame_width, frame_height)
-        self.__middle_cam.setInterleaved(False)
-        self.frame_width = frame_width
-        self.frame_height = frame_height
-
-        # Optional configuration
-        if(fps_limit is not None):
-            self.__middle_cam.setFps(fps_limit)
-        # ... other optional parameters
+        self.middle_cam = self.__pipeline.createColorCamera()
+        self.middle_cam.setPreviewSize(frame_width, frame_height)
 
         # Set output stream
         self.__middle_cam_output_stream = self.__pipeline.createXLinkOut()
         self.__middle_cam_output_stream.setStreamName(middle_cam_stream_name)
-        self.__middle_cam.preview.link(self.__middle_cam_output_stream.input)
+        self.middle_cam.preview.link(self.__middle_cam_output_stream.input)
+
+    
+    def setDepth(self, sensor_resolution=dai.MonoCameraProperties.SensorResolution.THE_400_P):
+        # Configure cameras
+        self.left_cam = self.__pipeline.createMonoCamera()
+        self.left_cam.setResolution(sensor_resolution)
+        self.left_cam.setBoardSocket(dai.CameraBoardSocket.LEFT)
+        self.right_cam = self.__pipeline.createMonoCamera()
+        self.right_cam.setResolution(sensor_resolution)
+        self.right_cam.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+
+        # Set depth
+        self.stereo = self.__pipeline.createStereoDepth()
+        self.left_cam.out.link(self.stereo.left)
+        self.right_cam.out.link(self.stereo.right)
+
+    
+    def __setSpatialLocationCalculator(self):
+        # Configure spatial location calculator
+        self.spatial_location_calculator = self.__pipeline.createSpatialLocationCalculator()
+        self.stereo.depth.link(self.spatial_location_calculator.inputDepth)
+        
+        # Set spatial location calculator input/output stream
+        self.__spatial_calculator_input_stream = self.__pipeline.createXLinkIn()
+        self.__spatial_calculator_input_stream.setStreamName(spatial_calculator_input_stream_name)
+        self.__spatial_calculator_input_stream.out.link(self.spatial_location_calculator.inputConfig)
+        self.__spatial_calculator_output_stream = self.__pipeline.createXLinkOut()
+        self.__spatial_calculator_output_stream.setStreamName(spatial_calculator_output_stream_name)
+        self.spatial_location_calculator.out.link(self.__spatial_calculator_output_stream.input)
 
 
-    def setModel(self, path, labels=None):
+    def setModel(self, path, link_middle_cam=True, link_depth=True):
+        # Warn user
+        if(link_middle_cam and self.middle_cam is None):
+            print("To link the middle camera you should configure it (call setMiddleCamera), skipped..")
+        if(link_depth and self.stereo is None):
+            print("To link the depth you should configure it (call setDepth), skipped..")
+        
         # Needed configuration
-        self.__nn = self.__pipeline.createNeuralNetwork()
-        self.__nn.setBlobPath(path)
-
-        # Optional configuration
-        if(labels is not None):
-            self.labels = labels
-        # ... other optional parameters
+        self.nn = self.__pipeline.createNeuralNetwork()
+        self.nn.setBlobPath(path)
 
         # Link middle camera to neural network input
-        if(self.__middle_cam is not None):
-            self.__middle_cam.preview.link(self.__nn.input)
-            self.__nn.passthrough.link(self.__middle_cam_output_stream.input)
+        if(link_middle_cam and self.middle_cam is not None):
+            self.middle_cam.preview.link(self.nn.input)
+            self.nn.passthrough.link(self.__middle_cam_output_stream.input)
+
+        # Link depth to neural network input
+        if(link_depth and self.stereo is not None):
+            self.__setSpatialLocationCalculator()
 
         # Set output stream
         self.__nn_output_stream = self.__pipeline.createXLinkOut()
         self.__nn_output_stream.setStreamName(nn_stream_name)
-        self.__nn.out.link(self.__nn_output_stream.input)
+        self.nn.out.link(self.__nn_output_stream.input)
         
 
-    def run(self, process, middle_cam_queue_size=1, nn_queue_size=1):
+    def run(self, process, middle_cam_queue_size=1, block_middle_cam_queue=False, nn_queue_size=1, block_nn_queue=False, spatial_calculator_input_queue_size=1, block_spatial_calculator_input_queue=False, spatial_calculator_output_queue_size=1, block_spatial_calculator_output_queue=False):
         # Find the connected device (example: OAK-D)
         with dai.Device(self.__pipeline) as device:
             device.startPipeline()
 
             # Define queues
-            if(self.__middle_cam is not None):
-                self.middle_cam_output_queue = device.getOutputQueue(name=middle_cam_stream_name, maxSize=middle_cam_queue_size, blocking=False)
-            if(self.__nn is not None):
-                self.nn_output_queue = device.getOutputQueue(name=nn_stream_name, maxSize=nn_queue_size, blocking=False)
+            if(self.middle_cam is not None):
+                self.middle_cam_output_queue = device.getOutputQueue(name=middle_cam_stream_name, maxSize=middle_cam_queue_size, blocking=block_middle_cam_queue)
+            if(self.nn is not None):
+                self.nn_output_queue = device.getOutputQueue(name=nn_stream_name, maxSize=nn_queue_size, blocking=block_nn_queue)
+            if(self.stereo is not None):
+                self.spatial_calculator_input_queue = device.getInputQueue(name=spatial_calculator_input_stream_name, maxSize=spatial_calculator_input_queue_size, blocking=block_spatial_calculator_input_queue)
+                self.spatial_calculator_output_queue = device.getOutputQueue(name=spatial_calculator_output_stream_name, maxSize=spatial_calculator_output_queue_size, blocking=block_spatial_calculator_output_queue)
 
             # Enter into processing loop
             while True:
--- IV-generalization/example.py
+++ IV-generalization/example.py
@@ -1,21 +1,29 @@
 from OakSingleModelRunner import OakSingleModelRunner
 from pathlib import Path
+import depthai as dai
 import cv2
 
 
 def main():
     # Set few parameters
     nn_path = str(Path(__file__).parent) + "/../models/coronamask.blob"
-    labelMap = ["background", "no mask", "mask", "no mask"]
+    labels = ["background", "no mask", "mask", "no mask"]
 
     # Init pipeline
     oak_detection = OakSingleModelRunner() 
 
     # Configure middle camera and init output streams
-    oak_detection.setMiddleCamera(frame_width=300, frame_height=300, fps_limit=20)
+    oak_detection.setMiddleCamera(frame_width=300, frame_height=300)
+    oak_detection.middle_cam.setInterleaved(False)
+    oak_detection.middle_cam.setFps(20)
+
+    # Configure stereo depth
+    oak_detection.setDepth()
 
     # Configure neural network model and init input / output streams
-    oak_detection.setModel(path=nn_path, labels=labelMap)
+    oak_detection.setModel(path=nn_path)
+    oak_detection.labels = labels
+    oak_detection.spatial_location_calculator.setWaitForConfigInput(True)
 
     # Run the loop that call the process function
     oak_detection.run(process=process, middle_cam_queue_size=4, nn_queue_size=4)
@@ -24,6 +32,8 @@ def main():
 # Process function
 def process(oak_detection):
     color = (255,0,0)
+    frame_width = oak_detection.middle_cam.getPreviewWidth()
+    frame_height = oak_detection.middle_cam.getPreviewHeight()
     frame = oak_detection.middle_cam_output_queue.get().getCvFrame()
     tensor = oak_detection.nn_output_queue.get().getLayerFp16("DetectionOutput")
     
@@ -32,11 +42,26 @@ def process(oak_detection):
         if (tensor[i*7 + 2] >0.5): # 3rd value of each detection is the confidence
             keeped_roi.append(tensor[i*7:i*7+7])
 
+    spatial_calculator_config = dai.SpatialLocationCalculatorConfig()
     for  id, label, confidence, left, top, right, bottom  in  keeped_roi:
-        topleft = (int(left*oak_detection.frame_width), int(top*oak_detection.frame_height))
-        bottomright = (int(right*oak_detection.frame_width), int(bottom*oak_detection.frame_height))
+        topleft = (int(left*frame_width), int(top*frame_height))
+        bottomright = (int(right*frame_width), int(bottom*frame_height))
         cv2.rectangle(frame, topleft, bottomright, color, 2) # ROI
         cv2.putText(frame, oak_detection.labels[int(label)] + f" {int(confidence * 100)}%", (topleft[0] + 10, topleft[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color) # Label and confidence
+
+        # Add ROIs to spatial location calculator config
+        spatial_config_data = dai.SpatialLocationCalculatorConfigData()
+        spatial_config_data.roi = dai.Rect(dai.Point2f(topleft[0], topleft[1]), dai.Point2f(bottomright[0], bottomright[1]))
+        spatial_calculator_config.addROI(spatial_config_data)
+
+    # Put spatial location info inside of the ROI
+    if(len(keeped_roi)>0):
+        oak_detection.spatial_calculator_input_queue.send(spatial_calculator_config)
+        spatial_data = oak_detection.spatial_calculator_output_queue.get().getSpatialLocations()
+        for depth_data in spatial_data:
+            cv2.putText(frame, f"X: {int(depth_data.spatialCoordinates.x)} mm", (topleft[0] + 10, topleft[1] + 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+            cv2.putText(frame, f"Y: {int(depth_data.spatialCoordinates.y)} mm", (topleft[0] + 10, topleft[1] + 65), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+            cv2.putText(frame, f"Z: {int(depth_data.spatialCoordinates.z)} mm", (topleft[0] + 10, topleft[1] + 80), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
 
     cv2.imshow("output", frame)
 
