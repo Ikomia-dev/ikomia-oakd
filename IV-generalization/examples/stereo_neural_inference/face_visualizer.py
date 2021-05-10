12142db8f05a1c0c5a2bf665d5b3fac8ab56f68a
ThomasPDM <thomas.pelissie@laposte.net>
2021-05-10 16:20:35 +0200

    handle stereo depth with runner and add few examples

---
+++ IV-generalization/examples/stereo_neural_inference/face_visualizer.py
@@ -0,0 +1,99 @@
+from pathlib import Path
+import depthai as dai
+import numpy as np
+import cv2
+import sys
+
+# Importing from parent folder
+sys.path.insert(0, str(Path(__file__).parent.parent.parent)) # move to parent path
+from utils.compute import to_planar, get_landmark_3d, get_vector_intersection
+from utils.visualize import LandmarksCubeVisualizer, LandmarksDepthVisualizer
+from utils.draw import drawROI, displayFPS
+from utils.OakRunner import OakRunner
+
+
+
+frame_width, frame_height = 300, 300
+visualizeLandmarksCube = True # define visualization mode (cube with centered face or cameras fields of views)
+
+
+
+# Function called before entering inside the process loop, useful to set few arguments
+def init(runner):
+    runner.custom_arguments["required_confidence"] = 0.2
+
+    colors = [(255,255,255), (255,255,255), (0,255,255), (255,0,255), (255,0,255)]
+    pairs = [(0,2), (1,2), (3,4)]
+    if(visualizeLandmarksCube):
+        runner.custom_arguments["visualizer"] = LandmarksCubeVisualizer(frame_width, frame_height, [runner.left_camera_location, runner.right_camera_location], colors=colors, pairs=pairs)
+    else:
+        runner.custom_arguments["visualizer"] = LandmarksDepthVisualizer(frame_width*2, frame_height, [runner.left_camera_location, runner.right_camera_location], colors=colors, pairs=pairs)
+    runner.custom_arguments["visualizer"].start()
+
+
+# Function called inside the process loop, useful to apply any treatment
+def process(runner):
+    spatial_vectors = dict()
+    for side in ["left", "right"]:
+        spatial_vectors[side] = []
+        frame = runner.output_queues[side+"_cam"].get().getCvFrame()
+        faces_data = runner.output_queues["nn_"+side+"_faces"].get().getFirstLayerFp16()
+
+        if(faces_data[2] > runner.custom_arguments["required_confidence"]):
+            # Get pixels instead of percentages
+            xmin = int(faces_data[3]*frame_width) if faces_data[3]>0 else 0
+            ymin = int(faces_data[4]*frame_height) if faces_data[4]>0 else 0
+            xmax = int(faces_data[5]*frame_width) if faces_data[5]<1 else frame_width
+            ymax = int(faces_data[6]*frame_height) if faces_data[6]<1 else frame_height
+
+            # Compute the face to get landmarks
+            land_data = dai.NNData()
+            planar_cropped_face = to_planar(frame[ymin:ymax, xmin:xmax], (48, 48))
+            land_data.setLayer("0", planar_cropped_face)
+            runner.input_queues["nn_"+side+"_landmarks"].send(land_data)
+            output = runner.output_queues["nn_"+side+"_landmarks"].get().getFirstLayerFp16()
+            landmarks = np.array(output).reshape(5,2)
+
+            # Draw detections
+            drawROI(frame, (xmin,ymin), (xmax,ymax), color=(0,200,230))
+            for x,y in landmarks:
+                cv2.circle(frame, (int(x*(xmax-xmin))+xmin,int(y*(ymax-ymin))+ymin), 2, (0,0,255))
+
+            # Set spatial vectors
+            spatial_landmarks = [get_landmark_3d((x,y)) for x,y in landmarks]
+            camera_location = runner.left_camera_location if(side == "left") else runner.right_camera_location
+            
+            for i in range(5):
+                spatial_vectors[side].append([spatial_landmarks[i][j] - camera_location[j] for j in range(3)])
+            spatial_vectors[side] = np.array(spatial_vectors[side]) # convert list to numpy array
+
+        displayFPS(frame, runner.getFPS())
+        cv2.imshow(side, frame)
+
+    # Determined depth to precisely locate landmarks in space
+    landmark_spatial_locations = []
+    if(len(spatial_vectors["left"])>4 and len(spatial_vectors["right"])>4):
+        for i in range(5):
+            landmark_spatial_locations.append(get_vector_intersection(spatial_vectors["left"][i], runner.left_camera_location, spatial_vectors["right"][i], runner.right_camera_location))
+
+        runner.custom_arguments["visualizer"].setLandmarks(landmark_spatial_locations)
+
+
+
+runner = OakRunner()
+
+for side in ["left", "right"]:
+    if(side == "left"):
+        runner.setLeftCamera(frame_width, frame_height)
+        face_manip = runner.getLeftCameraManip()
+    else:
+        runner.setRightCamera(frame_width, frame_height)
+        face_manip = runner.getRightCameraManip()
+
+    face_manip.initialConfig.setFrameType(dai.RawImgFrame.Type.BGR888p) # Switch to BGR (but still grayscaled)
+    runner.addNeuralNetworkModel(stream_name="nn_"+side+"_faces", path=str(Path(__file__).parent) + "/../../../models/face_detection.blob", handle_mono_depth=False)
+    face_manip.out.link(runner.neural_networks["nn_"+side+"_faces"].input) # link transformed video stream to neural network entry
+
+    runner.addNeuralNetworkModel(stream_name="nn_"+side+"_landmarks", path=str(Path(__file__).parent) + "/../../../models/landmarks.blob", handle_mono_depth=False)
+
+runner.run(process=process, init=init)
---
+++ IV-generalization/utils/OakRunner.py
@@ -0,0 +1,250 @@
+import depthai as dai
+import time
+import cv2
+
+
+
+class OakRunner:
+    def __init__(self):
+        self.__pipeline = dai.Pipeline()
+        self.__middle_cam = None
+        self.__right_cam = None
+        self.__left_cam = None
+        self.__stereo = None
+        self.__spatial_location_calculator = None
+        self.__right_cam_manip = None
+        self.__left_cam_manip = None
+
+        self.labels = dict()
+        self.neural_networks = dict()
+        self.input_queues = dict()
+        self.output_queues = dict()
+        self.left_camera_location = (0.107, -0.038, 0.008)
+        self.right_camera_location = (0.109, 0.039, 0.008)
+        self.custom_arguments = dict()
+
+        self.__framecount = 0
+        self.__starttime = None
+        self.__input_queue_arguments = dict()
+        self.__output_queue_arguments = dict()
+        
+        self.__spatial_location_calculator_stream_name = None
+        self.__middle_cam_stream_name = None
+        self.__right_cam_stream_name = None
+        self.__left_cam_stream_name = None
+
+
+    def __prepareStreamQueue(self, stream_name, queue_size, block_queue, dictionary):
+        dictionary[stream_name] = dict()
+        dictionary[stream_name]["maxSize"] = queue_size
+        dictionary[stream_name]["blocking"] = block_queue
+
+    def setMiddleCamera(self, frame_width, frame_height, stream_name="middle_cam", output_queue_size=1, block_output_queue=False):
+        # Needed configuration
+        self.__middle_cam = self.__pipeline.createColorCamera()
+        self.__middle_cam.setPreviewSize(frame_width, frame_height)
+
+        # Set output stream
+        middle_cam_output_stream = self.__pipeline.createXLinkOut()
+        middle_cam_output_stream.setStreamName(stream_name)
+        self.__middle_cam.preview.link(middle_cam_output_stream.input)
+        self.__middle_cam_stream_name = stream_name
+        self.__prepareStreamQueue(stream_name, output_queue_size, block_output_queue, self.__output_queue_arguments)
+        
+
+
+    def setRightCamera(self, frame_width, frame_height, sensor_resolution=dai.MonoCameraProperties.SensorResolution.THE_720_P, stream_name="right_cam", output_queue_size=1, block_output_queue=False):
+        # Init right cam
+        self.__right_cam = self.__pipeline.createMonoCamera()
+        self.__right_cam.setResolution(sensor_resolution)
+        self.__right_cam.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+
+        # Convert image output into proper sized output
+        self.__right_cam_manip = self.__pipeline.createImageManip()
+        self.__right_cam_manip.initialConfig.setResize(frame_width, frame_height)
+        self.__right_cam.out.link(self.__right_cam_manip.inputImage)
+
+        # Set output stream
+        right_cam_manip_output_stream = self.__pipeline.createXLinkOut()
+        right_cam_manip_output_stream.setStreamName(stream_name)
+        self.__right_cam_manip.out.link(right_cam_manip_output_stream.input)
+
+        # Prepare streaming queue
+        self.__right_cam_stream_name = stream_name
+        self.__prepareStreamQueue(stream_name, output_queue_size, block_output_queue, self.__output_queue_arguments)
+
+
+    def setLeftCamera(self, frame_width, frame_height, sensor_resolution=dai.MonoCameraProperties.SensorResolution.THE_720_P, stream_name="left_cam", output_queue_size=1, block_output_queue=False):
+        # Init left cam
+        self.__left_cam = self.__pipeline.createMonoCamera()
+        self.__left_cam.setResolution(sensor_resolution)
+        self.__left_cam.setBoardSocket(dai.CameraBoardSocket.LEFT)
+
+        # Convert image output into proper sized output
+        self.__left_cam_manip = self.__pipeline.createImageManip()
+        self.__left_cam_manip.initialConfig.setResize(frame_width, frame_height)
+        self.__left_cam.out.link(self.__left_cam_manip.inputImage)
+        
+        # Set output stream
+        left_cam_manip_output_stream = self.__pipeline.createXLinkOut()
+        left_cam_manip_output_stream.setStreamName(stream_name)
+        self.__left_cam_manip.out.link(left_cam_manip_output_stream.input)
+
+        # Prepare streaming queue
+        self.__left_cam_stream_name = stream_name
+        self.__prepareStreamQueue(stream_name, output_queue_size, block_output_queue, self.__output_queue_arguments)
+
+
+    def setMonoDepth(self, sensor_resolution=dai.MonoCameraProperties.SensorResolution.THE_720_P):
+        # Warn user
+        if((self.__left_cam is not None and self.__right_cam is None and self.__left_cam.getResolution() != sensor_resolution)
+         or (self.__left_cam is None and self.__right_cam is not None and sensor_resolution != self.__right_cam.getResolution())
+         or (self.__left_cam is not None and self.__right_cam is not None and self.__right_cam.getResolution() != self.__right_cam.getResolution())):
+            print("Can't handle mono depth, right and left cameras should have the same sensor resolution, skipped..")
+        else:
+            # Configure cameras
+            if(self.__left_cam is None):
+                self.__left_cam = self.__pipeline.createMonoCamera()
+                self.__left_cam.setResolution(sensor_resolution)
+                self.__left_cam.setBoardSocket(dai.CameraBoardSocket.LEFT)
+            if(self.__right_cam is None):
+                self.__right_cam = self.__pipeline.createMonoCamera()
+                self.__right_cam.setResolution(sensor_resolution)
+                self.__right_cam.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+
+            # Set depth
+            self.__stereo = self.__pipeline.createStereoDepth()
+            self.__left_cam.out.link(self.__stereo.left)
+            self.__right_cam.out.link(self.__stereo.right)
+
+
+    def __setSpatialLocationCalculator(self, stream_name):
+        # Configure spatial location calculator
+        self.__spatial_location_calculator = self.__pipeline.createSpatialLocationCalculator()
+        self.__stereo.depth.link(self.__spatial_location_calculator.inputDepth)
+
+        # Set spatial location calculator input/output stream
+        spatial_calculator_input_stream = self.__pipeline.createXLinkIn()
+        spatial_calculator_input_stream.setStreamName(stream_name)
+        spatial_calculator_input_stream.out.link(self.__spatial_location_calculator.inputConfig)
+        spatial_calculator_output_stream = self.__pipeline.createXLinkOut()
+        spatial_calculator_output_stream.setStreamName(stream_name)
+        self.__spatial_location_calculator.out.link(spatial_calculator_output_stream.input)
+
+
+    def __setModel(self, stream_name, path, input_queue_size, block_input_queue, output_queue_size, block_output_queue):
+        # Needed configuration
+        self.neural_networks[stream_name].setBlobPath(path)
+        self.__prepareStreamQueue(stream_name, input_queue_size, block_input_queue, self.__input_queue_arguments)
+        self.__prepareStreamQueue(stream_name, output_queue_size, block_output_queue, self.__output_queue_arguments)
+
+        # Set intput stream
+        nn_input_stream = self.__pipeline.createXLinkIn()
+        nn_input_stream.setStreamName(stream_name)
+        nn_input_stream.out.link(self.neural_networks[stream_name].input)
+
+        # Set output stream
+        nn_output_stream = self.__pipeline.createXLinkOut()
+        nn_output_stream.setStreamName(stream_name)
+        self.neural_networks[stream_name].out.link(nn_output_stream.input)
+
+
+    def addNeuralNetworkModel(self, stream_name, path, input_queue_size=1, block_input_queue=False, output_queue_size=1, block_output_queue=False, handle_mono_depth=True, spatial_calculator_stream_name="slc"):
+        self.neural_networks[stream_name] = self.__pipeline.createNeuralNetwork()
+        self.__setModel(stream_name, path, input_queue_size, block_input_queue, output_queue_size, block_output_queue)
+        if(handle_mono_depth):
+            if(self.__stereo is None): # Warn user
+                print("To link the mono depth you should configure it (call setMonoDepth), skipped..")
+            elif(self.spatial_location_calculator is None): # If necessary, init the spatial location calculator
+                self.__setSpatialLocationCalculator(spatial_calculator_stream_name)
+                print("There was no spatial_location_calculator, it has been set.")
+
+
+    def addMobileNetDetectionModel(self, stream_name, path, output_queue_size=1, block_output_queue=False, treshold=0.5, handle_mono_depth=True):
+        if(handle_mono_depth and self.__stereo is not None):
+            self.neural_networks[stream_name] = self.__pipeline.createMobileNetSpatialDetectionNetwork()
+            self.__stereo.depth.link(self.neural_networks[stream_name].inputDepth)
+        else:
+            if(handle_mono_depth): # Warn user
+                print("To link the mono depth you should configure it (call setMonoDepth), skipped..")
+            self.neural_networks[stream_name] = self.__pipeline.createMobileNetDetectionNetwork()
+        self.neural_networks[stream_name].setConfidenceThreshold(treshold)
+        self.__setModel(stream_name, path, input_queue_size, block_input_queue, output_queue_size, block_output_queue)
+
+
+    def addYoloDetectionModel(self, stream_name, path, num_classes, coordinate_size, anchors, anchor_masks, output_queue_size=1, block_output_queue=False, treshold=0.5, handle_mono_depth=True):
+        if(handle_mono_depth and self.__stereo is not None):
+            self.neural_networks[stream_name] = self.__pipeline.createYoloSpatialDetectionNetwork()
+            self.__stereo.depth.link(self.neural_networks[stream_name].inputDepth)
+        else:
+            if(handle_mono_depth): # Warn user
+                print("To link the mono depth you should configure it (call setMonoDepth), skipped..")
+            self.neural_networks[stream_name] = self.__pipeline.createYoloDetectionNetwork()
+        self.neural_networks[stream_name].setConfidenceThreshold(treshold)
+        self.neural_networks[stream_name].setNumClasses(num_classes)
+        self.neural_networks[stream_name].setCoordinateSize(coordinate_size)
+        self.neural_networks[stream_name].setAnchors(anchors)
+        self.neural_networks[stream_name].setAnchorMasks(anchor_masks)
+        self.__setModel(stream_name, path, input_queue_size, block_input_queue, output_queue_size, block_output_queue)
+
+    
+    def getFPS(self):
+        if(self.__starttime):
+            return self.__framecount / (time.time() - self.__starttime)
+        else:
+            return 0
+
+    def getPipeline(self):
+        return self.__pipeline
+
+    def getMiddleCamera(self):
+        return self.__middle_cam
+
+    def getRightCamera(self):
+        return self.__right_cam
+
+    def getLeftCamera(self):
+        return self.__left_cam
+
+    def getRightCameraManip(self):
+        return self.__right_cam_manip
+
+    def getLeftCameraManip(self):
+        return self.__left_cam_manip
+
+    def getStereo(self):
+        return self.__stereo
+
+    def getSpatialLocationCalculator(self):
+        return self.__spatial_location_calculator
+
+
+    def run(self, process, init=None):
+        # Find the connected device (example: OAK-D)
+        with dai.Device(self.__pipeline) as device:
+            device.startPipeline()
+
+            # Define queues
+            if(self.__middle_cam_stream_name is not None):
+                self.output_queues[self.__middle_cam_stream_name] = device.getOutputQueue(name=self.__middle_cam_stream_name, maxSize=self.__output_queue_arguments[self.__middle_cam_stream_name]["maxSize"], blocking=self.__output_queue_arguments[self.__middle_cam_stream_name]["blocking"])
+            if(self.__right_cam_stream_name is not None):
+                self.output_queues[self.__right_cam_stream_name] = device.getOutputQueue(name=self.__right_cam_stream_name, maxSize=self.__output_queue_arguments[self.__right_cam_stream_name]["maxSize"], blocking=self.__output_queue_arguments[self.__right_cam_stream_name]["blocking"])
+            if(self.__left_cam_stream_name is not None):
+                self.output_queues[self.__left_cam_stream_name] = device.getOutputQueue(name=self.__left_cam_stream_name, maxSize=self.__output_queue_arguments[self.__left_cam_stream_name]["maxSize"], blocking=self.__output_queue_arguments[self.__left_cam_stream_name]["blocking"])
+            for name in self.neural_networks.keys():
+                self.input_queues[name] = device.getInputQueue(name=name, maxSize=self.__input_queue_arguments[name]["maxSize"], blocking=self.__input_queue_arguments[name]["blocking"])
+                self.output_queues[name] = device.getOutputQueue(name=name, maxSize=self.__output_queue_arguments[name]["maxSize"], blocking=self.__output_queue_arguments[name]["blocking"])
+            if(self.__spatial_location_calculator_stream_name is not None):
+                self.input_queues[self.__spatial_location_calculator_stream_name] = device.getInputQueue(name=self.__spatial_location_calculator_stream_name, maxSize=self.__input_queue_arguments[self.__spatial_location_calculator_stream_name]["maxSize"], blocking=self.__input_queue_arguments[self.__spatial_location_calculator_stream_name]["blocking"])
+                self.output_queues[self.__spatial_location_calculator_stream_name] = device.getOutputQueue(name=self.__spatial_location_calculator_stream_name, maxSize=self.__output_queue_arguments[self.__spatial_location_calculator_stream_name]["maxSize"], blocking=self.__output_queue_arguments[self.__spatial_location_calculator_stream_name]["blocking"])
+
+            if(init is not None):
+                init(self) # Call the init function
+
+            self.__starttime = time.time()
+            # Enter into processing loop
+            while True:
+                process(self) # Call the process function
+                self.__framecount += 1
+                if cv2.waitKey(1) == ord('q'):
+                    break
